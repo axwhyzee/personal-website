5808344856646e62d3cfb76f02b5dd0fbe6d7d97
axwhyzee <siah_weehung@yahoo.com>
2023-07-09 00:29:53 +0800

    external LC api wrapper

--- frontend/src/utils/leetcode_api.js
+++ frontend/src/utils/leetcode_api.js
@@ -1,16 +1,7 @@
-import { SERVER_URL, CONTENT_TYPE_HEADERS } from '../consts/consts';
-import { QUERY_USER_STATS } from '../consts/graphql/leetcode';
+import { LEETCODE_GRAPHQL_URL } from '../consts/consts';
 
 export async function get_user_stats(user) {
-    const r = await fetch(`${SERVER_URL}/proxy/graphql/leetcode`, {
-        method: 'POST',
-        headers: CONTENT_TYPE_HEADERS,
-        body: JSON.stringify({
-            query: QUERY_USER_STATS,
-            variables: {login: user}
-        })
-    });
-    const data = await r.json();
-    console.log(data);
-    return data;
+    console.log(user);
+    const r = await fetch(`${LEETCODE_GRAPHQL_URL}/${user}`);
+    return await r.json();
 }
