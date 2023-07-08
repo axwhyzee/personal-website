018e2b8f1776c93d8f3f0f27577dbc85d6cad434
axwhyzee <siah_weehung@yahoo.com>
2023-07-09 00:30:12 +0800

    external LC api url

--- frontend/src/consts/consts.js
+++ frontend/src/consts/consts.js
@@ -13,3 +13,5 @@ export const SERVER_URL = 'https://axwhyzee-website-server.onrender.com';
 export const RESUME_FILEPATH = `${SERVER_URL}/file?filepath=00000000-0000-0000-0000-000000000000.pdf`;
 
 export const RESUME_FILENAME = 'resume_siah_wee_hung.pdf';
+
+export const LEETCODE_GRAPHQL_URL = 'https://leetcode-stats-api.herokuapp.com'
