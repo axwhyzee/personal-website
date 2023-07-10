d8957497c8761cd6cd6107fd0be5e2c0f091596a
axwhyzee <siah_weehung@yahoo.com>
2023-07-10 16:14:23 +0800

    integrate new Input component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,37 +5,49 @@ import Sidebar from './components/sidebar/Sidebar.jsx';
 import Spinner from './components/spinner/Spinner.jsx';
 
 import { list_user_repos } from './utils/github_api.js';
-import { Githubsearch } from './githubsearch/Githubsearch.jsx';
+import Githubsearch from './components/githubsearch/Githubsearch.jsx';
 import './app.css'
 
 function App() {
     const [repos, setRepos] = useState([]);
     const [pinned, setPinned] = useState([]);
-    const [username, setUsername] = useState('axwhyzee');
+    const [username, setUsername] = useState('');
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
-    useEffect(() => {
-        const list_repos = async () => {
-            const fetched_repos = await list_user_repos(username);
+    const list_repos = async (newUsername, token='') => {
+        setLoading(true);
+        setUsername(newUsername)
+
+        const res = await list_user_repos(newUsername, token);
+
+        if (res.error) {
+            setError(res.error);
+        } else {
             const not_pinned = [];
             const is_pinned = [];
-            for (const repo of fetched_repos) {
+            for (const repo of res.repos) {
                 if (repo.pinned) is_pinned.push(repo);
                 else not_pinned.push(repo);
             }
             setRepos(not_pinned);
             setPinned(is_pinned);
-            setLoading(false);
         }
-        setLoading(true);
-        list_repos();
-    }, [username])
+        setLoading(false);
+    }
 
-    const setUsernameCallback = (s) => {
-        if (s !== username) {
+    useEffect(() => {
+        list_repos('axwhyzee');
+    }, [])
+
+    const setUsernameTokenCallback = (s) => {
+        console.log(s);
+        const parts = s.split(';');
+        console.log(parts);
+        if (parts[0]) {
             setRepos([]);
             setPinned([]);
-            setUsername(s);
+            list_repos(parts[0], parts[1]);
         }
     }
 
@@ -43,7 +55,7 @@ function App() {
         <div className='App'>
             <div className='main-view'>
                 <div className='box-shadow'>
-                    <Githubsearch setUsername={setUsernameCallback} />
+                    <Githubsearch callback={setUsernameTokenCallback} />
                 </div>
                 <div className='spacer' />
                 <div className='cards bg-light box-shadow'>
@@ -59,6 +71,7 @@ function App() {
                 <div className='spacer' />
                 <div className='cards bg-dark box-shadow'>
                     {repos.length > 0 ? repos.map((repo, i) => <Card key={i} repo={repo}/>) : <h4 className='header'>No repositories found</h4>}
+                    {error && <span className='error'>{error}</span>}
                 </div>
             </div>
             <Sidebar />
