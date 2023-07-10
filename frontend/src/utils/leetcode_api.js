03d8442852334d4dd3ad287ce7e6e4543fca46cd
axwhyzee <siah_weehung@yahoo.com>
2023-07-10 16:12:57 +0800

    remove console.log

--- frontend/src/utils/leetcode_api.js
+++ frontend/src/utils/leetcode_api.js
@@ -1,7 +1,6 @@
 import { LEETCODE_GRAPHQL_URL } from '../consts/consts';
 
 export async function get_user_stats(user) {
-    console.log(user);
     const r = await fetch(`${LEETCODE_GRAPHQL_URL}/${user}`);
     return await r.json();
 }
