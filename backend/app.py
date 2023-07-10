99687b9a004595aac86a7d94b10154250db9bbb7
axwhyzee <siah_weehung@yahoo.com>
2023-07-10 16:10:54 +0800

    docx parser, list and delete files w token auth

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 import os
 
 from bardapi import Bard 
-from fastapi import FastAPI, File, UploadFile, status
+from fastapi import FastAPI, File, Header, Request, status, UploadFile
 from fastapi.responses import FileResponse, Response, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-from typing import Literal
+from typing import Literal, List
 import uvicorn
 
 from settings import (
@@ -16,7 +16,8 @@ from settings import (
     PORT,
     UPLOAD_FILEPATH
 )
-from utils.pdf_reader import pdf_to_text
+from utils.auth import auth_request
+from utils.parse_file import read_text, read_pdf, read_docx
 from utils.filepaths import clean_filepath, to_uuid_filename
 from models.payload import GraphQLPayload
 
@@ -25,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*', 'http://localhost:3000'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
@@ -54,19 +55,17 @@ def get_file(
 def bard_inference_on_file(
     filepath: str
 ) -> JSONResponse:
-    text = ''
     filepath = os.path.join(UPLOAD_FILEPATH, filepath)
-    if filepath.endswith('.pdf'):
-        text = pdf_to_text(filepath)
-    elif filepath.endswith('.txt'):
-        with open(filepath, 'r') as f:
-            text = f.read()
-    elif filepath.endswith('.docx'):
-        pass
-    return JSONResponse(
-        content=bard.get_answer(text + APPEND_PROMPT)['content'] if text else 'No content found in file.',
-        status_code=status.HTTP_200_OK
-    )
+    f = read_pdf if filepath.endswith('.pdf') else \
+        read_text if filepath.endswith('.txt') else \
+        read_docx if filepath.endswith('.docx') else None
+    if f:
+        return JSONResponse(
+            content=bard.get_answer(f(filepath) + APPEND_PROMPT)['content'],
+            status_code=status.HTTP_200_OK
+        )
+    else:
+        return JSONResponse(content='No content found in file.', status_code=status.HTTP_200_OK)
     
 
 @app.post('/file/upload')
@@ -93,14 +92,43 @@ def upload_resume(
         file.file.close()
 
 
+@app.get('/file/list')
+def list_files(
+    r: Request
+) -> JSONResponse:
+    if auth_request(r):
+        return JSONResponse(content=os.listdir('uploaded'))
+    else:
+        return JSONResponse(content={'error': 'Invalid credenials'}, status_code=status.HTTP_401_UNAUTHORIZED)
+    
+
+@app.get('/file/delete')
+def list_files(
+    files: List[str],
+    r: Request
+) -> JSONResponse:
+    if auth_request(r):
+        res = {'deleted': [], 'invalid': []}
+        for file in files:
+            try:
+                os.remove(os.path.join(UPLOAD_FILEPATH, file))
+                res['deleted'].append(file)
+            except FileNotFoundError:
+                res['invalid'].append(file)
+        return JSONResponse(content=res, status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(content={'error': 'Invalid credenials'}, status_code=status.HTTP_401_UNAUTHORIZED)
+
+
 @app.post('/proxy/graphql/{dest}')
 def forward_request(
+    r: Request,
     payload: GraphQLPayload,
     dest: Literal['github', 'leetcode']
 ) -> JSONResponse:
     headers = {'Content-Type': 'application/json'}
     if dest=='github':
-        headers['Authorization'] = 'Bearer ' + GITHUB_API_KEY
+        headers['Authorization'] = r.headers.get('Authorization', 'Bearer ' + GITHUB_API_KEY)
         url=GITHUB_GRAPHQL_URL
     elif dest=='leetcode':
         url=LEETCODE_GRAPHQL_URL
@@ -113,9 +141,7 @@ def forward_request(
         headers=headers,
         json=vars(payload)
     )
-    print(r.text)
     return JSONResponse(content=r.json(), status_code=status.HTTP_200_OK)
-
 
 if __name__ == '__main__':
     uvicorn.run('app:app', host='0.0.0.0', port=PORT)
