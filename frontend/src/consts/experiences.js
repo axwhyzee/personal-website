00c567a7b1a6f4c0f442f8cf7f90c272fa3a46f4
axwhyzee <siah_weehung@yahoo.com>
2023-07-10 22:29:32 +0800

    Create experiences.js

---
+++ frontend/src/consts/experiences.js
@@ -0,0 +1,37 @@
+const experiences = [
+    {
+        title: 'Software Developer',
+        company: 'Ion Mobility',
+        time: 'May 2023',
+        description: [
+            'Custom Xendit payment gateway plugin w/ Saleor ecommerce backend (Django)',
+            'Proxy server, CI/CD automated testing w/ mocks',
+            'Implement & deploy project management tool based on org’s GitHub ProjectV2 data',
+            'Ad hoc React projects for product demos'
+        ]
+    },
+    {
+        title: 'Tech Business Analyst',
+        company: 'Panasonic R&D',
+        time: 'May 2022 - Jul 2022',
+        description: [
+            'Custom web scraper w/ Selenium. Dynamically scrapes 200+ different companywebsites, >70% client extraction rate',
+            'NLP keyword extraction & filtering',
+            'Data analysis & recommendations based on constructed tech landscape'
+        ]
+    },
+    {
+        title: 'Data Management',
+        company: 'Bank of Singapore, Operations',
+        time: 'Apr 2021 - Jul 2021',
+        description: ['Built & distributed a fully functional automation software that saves 5-7 hours per day']
+    },
+    {
+        title: 'Project Admin',
+        company: 'Bank of Singapore, Digital Transformation',
+        time: 'Feb 2021 - Apr 2021',
+        description: ['Digitalized bank forms using Adobe Acrobat & JavaScript']
+    }
+]
+
+export default experiences;
